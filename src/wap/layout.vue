66afa603246ab7848fec7dbfd72d954e34167448
ttflowerboys <ttflowerboys@gmail.com>
2018-08-08 17:01:50 +0800

    [wap] Mod: layout.

---
+++ src/wap/layout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="Layout" class="Layout">
+        <div id="Main" class="Main">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'wap',
+        data() {
+            return {}
+        }
+    }
+</script>
